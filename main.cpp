5109469747e97367357e9f39aca9cf1e63e455d9
David Mattia <dmattia@nd.edu>
2015-03-09 14:02:26 -0400

    Added background image

--- main.cpp
+++ main.cpp
@@ -4,41 +4,70 @@
 #include "SDL/SDL.h"
 #include <cmath>
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(int argc, char* args[]) {
-	Sheep sheep(5,12,1.0,M_PI / 2.0);
+SDL_Surface *load_image(string filename) {
+	SDL_Surface * loadedImage = NULL;	
+	SDL_Surface * optimizedImage = NULL;
+	loadedImage = SDL_LoadBMP( filename.c_str() );
+	if(loadedImage) {
+		optimizedImage = SDL_DisplayFormat(loadedImage);
+		SDL_FreeSurface(loadedImage);
+	} else {
+		cout << "Could not load " << filename << endl;
+	}
+	return optimizedImage; //NULL if no image loaded
+}
+
+void apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination) {
+	SDL_Rect offset;
+	offset.x = x;
+	offset.y = y;
 	
-	SDL_Surface* hello = NULL;
+	SDL_BlitSurface(source, NULL, destination, &offset);
+}
+
+int main(int argc, char* args[]) {
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
+	const int SCREEN_BPP = 32;
+	
+	//Images to load
+	SDL_Surface* background = NULL;
+	SDL_Surface* message = NULL;
     	SDL_Surface* screen = NULL;          
 
-	//Start SDL
-	SDL_Init( SDL_INIT_EVERYTHING );
-	
-        //Set up screen
-        screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
-	
-	//Load image
-        if ( hello = SDL_LoadBMP( "sheep.bmp" ) ) {
-		cout << "loaded successfully" << endl;
-	} else {
-		cout << "load failed" << endl;
-		return 0;
+	// load SDL components
+	if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) {
+        	return 1; //ERROR 1: could not initialize SDL objects 
 	}
- 
-	//Apply image to screen
-	SDL_BlitSurface( hello, NULL, screen, NULL );
-	
-	//Update Screen
-	SDL_Flip( screen );
-	
-	//Pause
-	SDL_Delay( 2000 );
 
-	//Free the loaded image
-	SDL_FreeSurface( hello );
+	// setup screen, the window all other graphics will go into
+	screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+
+	if (!screen) {
+		return 2; //ERROR 2: could not initialize screen
+	}
 	
-	//Quit SDL
+	//Set window caption
+	SDL_WM_SetCaption( "Sheep Herder", NULL );
+
+	background = load_image("images/background.bmp");
+	message = load_image( "images/hello.bmp" );
+
+	apply_surface(0,0,background,screen);
+	apply_surface(320, 0, background, screen);
+	apply_surface(0, 240, background, screen);
+	apply_surface(320, 240, background, screen);
+	apply_surface(180, 140, message, screen);
+	
+	SDL_Flip(screen);
+	SDL_Delay(5000);
+	SDL_FreeSurface(background);
+	SDL_FreeSurface(message);
+	SDL_FreeSurface(screen);
+
 	SDL_Quit();
 	
         return 0; 
