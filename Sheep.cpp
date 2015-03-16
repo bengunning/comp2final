9fb092ce6b43ac8e435c9e5cd41725d642305c39
David Mattia <dmattia@nd.edu>
2015-03-16 13:52:28 -0400

    Shortened the == operator for Sheep

--- Sheep.cpp
+++ Sheep.cpp
@@ -7,11 +7,7 @@ Sheep::Sheep(int xPos, int yPos, double speed, double direction):
 	{}
 
 int Sheep::operator==(const Sheep& another) {
-	if (xPos == another.xPos && yPos == another.yPos) {
-		return 1;
-	} else {
-		return 0;
-	}
+	return (xPos == another.xPos && yPos == another.yPos); 
 }
 // This definition of equality makes the assumption that different sheep cannot share the exact same horizontal and vertical coordinates
 
