094aa5a35840b7e22464795d69e5b82b7c77fa1f
JohnNolan5 <jnolan5@student03.cse.nd.edu>
2015-03-17 12:07:13 -0400

    Added an SDL_Surface to sheep objects and a way to handle mouse click events, calculating a new velocity and direction.

--- Sheep.cpp
+++ Sheep.cpp
@@ -1,11 +1,19 @@
 // Implementation of class Sheep
 
 #include "Sheep.h"
+#include "SDL/SDL.h"
+#include <cmath> // for calculating direction radians from coordinates
 
-Sheep::Sheep(int xPos, int yPos, double speed, double direction):
-	xPos(xPos), yPos(yPos), speed(speed), direction(direction)
+Sheep::Sheep(int xPos, int yPos, double speed, double direction, SDL_Surface *pic):
+	xPos(xPos), yPos(yPos), speed(speed), direction(direction), picture(pic)
 	{}
 
+Sheep::~Sheep() {
+	if (picture) SDL_FreeSurface(picture); // free the sheep's individual surface pointer, don't quit
+}
+SDL_Surface *Sheep::getPicture() {
+	return picture;
+}
 int Sheep::operator==(const Sheep& another) {
 	return (xPos == another.xPos && yPos == another.yPos); 
 }
@@ -17,4 +25,16 @@ int Sheep::getX() {
 
 int Sheep::getY() {
 	return yPos;
+}
+
+void Sheep::handleEvents(SDL_Event* e) {
+
+	if (e->type == SDL_MOUSEBUTTONDOWN) { // user has clicked
+		int xMouse, yMouse;
+		SDL_GetMouseState(&xMouse, &yMouse); // set x y mouse positions
+		direction = atan2(-(yPos - yMouse), (xPos - xMouse)); // negate y to accomodate for inverted coordinates. does pi/2 need to be
+		// the top of the screen
+		speed += 100/sqrt(pow(xPos - xMouse, 2) + pow(yPos - yMouse, 2)); // increase speed by arbitary value over the distance in pixels
+	}
+	
 }
--- Sheep.h
+++ Sheep.h
@@ -5,18 +5,25 @@
 
 #ifndef SHEEP_H
 #define SHEEP_H
+#include "SDL/SDL.h"
 
 class Sheep {
 public:
-	Sheep(int=0,int=0,double=0,double=0); // Creates a sheep with a given position.
+	Sheep(int=0,int=0,double=0,double=0,SDL_Surface *pic = NULL); // Creates a sheep with a given position.
 						 // By default, a sheep is placed at the center of the screen facing right
+	~Sheep(); // Destructor to free the surface pointer
+	SDL_Surface *getPicture(); // return SDL_Surface of sheep pic to let graphics apply_surface draw it
+	// probably a felt or hand-knit image, or an image file with 72 different rotations, it depends on how we want to rotate
 	int operator==(const Sheep&); // Checks to see if two sheep share identical coordinates
-	int getX();
+	int getX(); // X and Y coordinates will give apply_surface the proper offset. 
 	int getY();
+	void handleEvents(SDL_Event*); // handle mouse clicks, updating velocity
+	//void updatePos(); // update next position based on speed and direction.
 private:
 	int xPos, yPos; // A sheeps position within a window.
 	double speed; // holds the speed of a sheep. Must be a positive value, 0 if not in motion
 	double direction; // A cartesian direction in radians. 0 is to the right of the screen, pi/2 is the top of the screen, etc.
+	SDL_Surface *picture; // picture to be drawn below sheeps coordinates  
 };
 
 #endif
