9725649529458908fae556fa982b4a6169b16c3d
Guixing Lin <glin1@student00.cse.nd.edu>
2015-03-21 22:49:26 -0400

    Update Herd class

--- Herd.cpp
+++ Herd.cpp
@@ -4,39 +4,76 @@
  */
 
 #include"Herd.h"
-#include<algorithm>
-#include<vector>
 using namespace std;
 
-void Herd::add( Sheep sheep ) {
-	group.push_back(sheep);
+// Constructor 
+Herd::Herd()
+{
+   list<Sheep> group;
+   numSheep = 0;
+   vector<vector<int> > locations;
 }
 
-void Herd::remove( Sheep sheep ) {
-	vector<Sheep>::iterator ptr;
-	ptr = find( group.begin() , group.end() , sheep );
-
-	if (ptr != group.end()) {
-		group.erase(ptr);
-	}
+// Add a sheep to the end of the group list
+void Herd::bear( Sheep& sheep )
+{
+   group.push_back(sheep);
+   numSheep ++;
+   // store the location of sheep into vector locations   
+   vector<int> temp; // temp vector to store location of sheep
+   temp.push_back(sheep.getX());
+   temp.push_back(sheep.getY());
+   locations.push_back(temp);
 }
 
+// Remove a particular sheep from the group list
+void Herd::shear( Sheep& sheep ) {
+   if(numSheep < 1) 
+      return;
+
+   list<Sheep>::iterator it = find( group.begin() , group.end() , sheep );
+   if (it != group.end()) {
+      group.erase(it);
+      numSheep --;
+      // remove the location of sheep from vector locations	 
+      for(int i = 0; i < numSheep; i ++)
+	 if(locations[i][0] == sheep.getX() && locations[i][1] == sheep.getY())
+	    locations.erase(locations.begin() + i);
+   }
+}
+
+// Calculate the return the horizontal average of all sheep in the group
 int Herd::xCenter() {
-	vector<Sheep>::iterator ptr;
+	list<Sheep>::iterator it;
 	int total = 0 , count = 0;
-	for (ptr = group.begin() ; ptr != group.end() ; ptr++) {
-		total += (*ptr).getX();
+	for (it = group.begin() ; it != group.end() ; it++) {
+		total += (*it).getX();
 		count++;
 	}
 	return total / count;
 }
 
+// Calculate the return the vertical average of all sheep in the group
 int Herd::yCenter() {
-	vector<Sheep>::iterator ptr;
+	list<Sheep>::iterator it;
 	int total = 0 , count = 0;
-	for (ptr = group.begin() ; ptr != group.end() ; ptr++) {
-		total += (*ptr).getY();
+	for (it = group.begin() ; it != group.end() ; it++) {
+		total += (*it).getY();
 		count++;
 	}
 	return total / count;
 }
+
+// Return locations of all sheep in the group list
+vector<vector<int> > Herd::getAllLocations()
+{
+   return locations;
+}
+
+// Call updatePos() function for all sheep in the group list
+void Herd::updateAll()
+{
+   for(list<Sheep>::iterator it = group.begin(); it != group.end(); it ++)
+      it->updatePos();
+}
+
--- Herd.h
+++ Herd.h
@@ -6,16 +6,25 @@
 #define HERD_H
 
 #include"Sheep.h"
-#include<vector>
+#include <list>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 using namespace std;
 
 class Herd {
-	public:
-		void add( Sheep ); // Add a sheep to the group vector
-		void remove( Sheep ); // Remove a particular sheep from the group vector
-		int xCenter(); // Calculate and return the horizontal average of all sheep in the group
-		int yCenter(); // Calculate and return the vertical average of all sheep in the group
-	private:
-		vector<Sheep> group;
+   public:
+      Herd();	     		    // Constructor
+      void bear( Sheep& );	    // Add a sheep to the end of the group list
+      void shear( Sheep& );	    // Remove a particular sheep from the group list
+      int xCenter();		    // Calculate and return the horizontal average position of all sheep in the group
+      int yCenter();		    // Calculate and return the vertical average position of all sheep in the group
+      vector<vector<int> > getAllLocations();   // Return locations of all the sheep in the group list
+      void updateAll();			     // Call updatePos() function for all sheep in the group list
+
+   private:
+      list<Sheep> group;	       // list of sheep
+      int numSheep;		       // number of sheep in the list
+      vector<vector<int> > locations;  // an n*2(xPos in 1st col and yPos in 2nd col) 2-D vector storing locations of all the sheep in the group list
 };
 #endif
